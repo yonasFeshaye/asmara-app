204cd307b2cc0c8267bd71c5dcf2d4cd72848306
Yonas <yonas.m.feshaye@hotmail.com>
2023-10-18 16:26:14 -0400

    Style the active link in the navbar

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -1,22 +1,30 @@
+"use client"
 import React from 'react'
+import { usePathname } from 'next/navigation'
 import Link from 'next/link'
 import {GiCamel} from 'react-icons/Gi'
+import classnames from 'classnames'
 
 
 const Navbar = () => {
+    const currentPath = usePathname()
     const links = [
         {label: 'Dashboard', href:'/'},
         {label: 'Issues', href:'/issues'}
     ]
   return (
     <nav className='flex space-x-6 border-b mb-5 px-5 h-14 items-center'>
-        <Link href="/"><GiCamel /></Link>
+        <Link href="/" className='hover:animate-bounce'><GiCamel /></Link>
         <ul className='flex space-x-6'>
             {links.map(link => 
             <Link 
                 key={link.href} 
                 href={link.href} 
-                className='text-zinc-400 hover:text-zinc-900     transition-colors'>
+                className={classnames({
+                    'text-zinc-900': link.href === currentPath,
+                    'text-zinc-500': link.href !== currentPath,
+                    'hover:text-zinc-800 transition-colors': true
+                })}>
                     {link.label}
             </Link>
                 )}                   
