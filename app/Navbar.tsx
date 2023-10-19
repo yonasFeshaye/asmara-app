e4552d01f1c9fbfc4ebd5fcddca215351eb7f376
Yonas Mekonnen Feshaye <yonas.m.feshaye@hotmail.com>
2023-10-18 23:41:56 -0400

    Build an API for creating issues

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -1,36 +1,38 @@
-"use client"
-import React from 'react'
-import { usePathname } from 'next/navigation'
-import Link from 'next/link'
-import {GiCamel} from 'react-icons/Gi'
-import classnames from 'classnames'
-
+"use client";
+import React from "react";
+import { usePathname } from "next/navigation";
+import Link from "next/link";
+import { GiCamel } from "react-icons/gi";
+import classnames from "classnames";
 
 const Navbar = () => {
-    const currentPath = usePathname()
-    const links = [
-        {label: 'Dashboard', href:'/'},
-        {label: 'Issues', href:'/issues'}
-    ]
+  const currentPath = usePathname();
+  const links = [
+    { label: "Dashboard", href: "/" },
+    { label: "Issues", href: "/issues" },
+  ];
   return (
-    <nav className='flex space-x-6 border-b mb-5 px-5 h-14 items-center'>
-        <Link href="/" className='hover:animate-bounce'><GiCamel /></Link>
-        <ul className='flex space-x-6'>
-            {links.map(link => 
-            <Link 
-                key={link.href} 
-                href={link.href} 
-                className={classnames({
-                    'text-zinc-900': link.href === currentPath,
-                    'text-zinc-500': link.href !== currentPath,
-                    'hover:text-zinc-800 transition-colors': true
-                })}>
-                    {link.label}
-            </Link>
-                )}                   
-        </ul>
+    <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
+      <Link href="/" className="hover:animate-bounce">
+        <GiCamel />
+      </Link>
+      <ul className="flex space-x-6">
+        {links.map((link) => (
+          <Link
+            key={link.href}
+            href={link.href}
+            className={classnames({
+              "text-zinc-900": link.href === currentPath,
+              "text-zinc-500": link.href !== currentPath,
+              "hover:text-zinc-800 transition-colors": true,
+            })}
+          >
+            {link.label}
+          </Link>
+        ))}
+      </ul>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
